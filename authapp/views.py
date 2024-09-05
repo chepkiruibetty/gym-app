bb79fcafdb1755537c4996395e1968e29d87cac8
chepkiruibetty <chepkiruibetty64@gmail.com>
2024-09-05 14:06:54 +0300

    created function for contact

--- authapp/views.py
+++ authapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
-
+from authapp.models import Contact
 # Create your views here.
 def Home(request):
     return render(request,"index.html")
@@ -72,5 +72,14 @@ def handlelogout(request):
      return redirect('/login')
      
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('fullname')
+        email=request.POST.get('email')
+        number=request.POST.get('num')
+        desc=request.POST.get('desc')
+        myquery=Contact(name=name,email=email,phonenumber=number,description=desc)
+        myquery.save()       
+        messages.info(request,"Thanks for Contacting us we will get back to you soon")
+        return redirect('/contact')
+        
     return render(request,"contact.html")
-     
