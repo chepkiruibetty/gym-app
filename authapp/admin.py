6b21d37f54b7c14c9a2466a24d32f3fc47507304
chepkiruibetty <chepkiruibetty64@gmail.com>
2024-09-05 17:20:47 +0300

    registered the models in the admin

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from authapp.models import Contact
+from authapp.models import Contact,MembershipPlan,Enrollment,Trainer
 # Register your models here.
 admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
